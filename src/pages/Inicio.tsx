0797787cfeb5f0b424fdd758fe7958b3a72d6ea3
alumno 4204 <alprepa@cedi.site>
2026-03-27 10:51:47 -0600

    27/03/23

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -54,10 +54,41 @@ const Inicio: React.FC = () => {
                         <h2 className='text-lg font-semibold'>Productos recientes</h2>
                         <div
                             onClick={() => history.push('/productos')}
+                            className='flex items-center gap-1 text-[#65d380] text-sm font-medium'
                         >
-                        Ver todo
+                        Ver todo <IonIcon icon={chevronForwardOutline}/>
                         </div>
                     </div>
+
+                {/* LISTA DE PRODUCTOS */}
+                <div className='space y-3'>
+                    {producto.length > 0 ? (
+                        // Si hay productos los muestra 
+                        producto.map((pros) => (
+                            <div className='bg-[#181a1f] p-4 rounded-3xl border border-gray-800 flex items-center justify-between'>
+                                <div className='flex items-center gap-4'>
+                                    <div className='bg-gray-900 p-3 rounded-2xl border border-gray-800'>
+                                        <IonIcon icon={cubeOutline} className='text-[#65d280] text-xl'/>
+                                    </div>
+
+                                    <div>
+                                        <h3 className='text-white font-bold text-sm'>{pros.nombre}</h3>
+                                        <p className='text-white text-sm'>{pros.categoria}</p>
+                                    </div>
+                                </div>
+                                {/* Precio y cantidad */}
+                                <div className='text-right'>
+                                    <p className='text-[#65d380] font-bold'>${Number(pros.precio).toFixed(2)}</p>
+                                    <p className='text-gray-500 text-xs'>{pros.cantidad} unidades</p>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <div className='bg-[#18a1f] p-10 rounded-3xl border border-gat-800 text-center'>
+                            <p className='text-gray-500 text-sm'>No hay productos registrados</p>
+                        </div>
+                    )}
+                </div>
                 </div>
             </div>
         </MainLayout>
